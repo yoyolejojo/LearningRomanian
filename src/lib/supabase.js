8b39f9622e119b360141e15f3aae03167b09473d
yoyolejojo <yohan.mebrouk@gmail.com>
2026-06-07 19:07:53 +0200

    Oue

--- src/lib/supabase.js
+++ src/lib/supabase.js
@@ -3,4 +3,7 @@ import { createClient } from '@supabase/supabase-js';
 const supabaseUrl = import.meta.env.PUBLIC_SUPABASE_URL;
 const supabaseKey = import.meta.env.PUBLIC_SUPABASE_ANON_KEY;
 
-export const supabase = createClient(supabaseUrl, supabaseKey);
+export const supabase = createClient(supabaseUrl, supabaseKey, {
+  realtime: { enabled: false },
+  global: { fetch },
+});
